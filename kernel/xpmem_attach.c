a477e8d2bc5a73f7faf56d6ecfd1fc9e7ebf70c7
Thomas Vegas <tvegas@nvidia.com>
2023-03-08 15:32:37 +0200

    Remove unused function

--- kernel/xpmem_attach.c
+++ kernel/xpmem_attach.c
@@ -267,53 +267,6 @@ out:
 	return result? : VM_FAULT_SIGBUS;
 }
 
-static int
-xpmem_map_pages(struct xpmem_segment *seg,
-				struct vm_area_struct *vma, u64 vaddr, unsigned long pfn)
-{
-	unsigned long old_pfn;
-	int ret = VM_FAULT_SIGBUS;
-
-	/*
-	 * remap_pfn_range() does not allow racing threads to each insert
-	 * the PFN for a given virtual address.  To account for this, we
-	 * call remap_pfn_range() with the att->mutex locked and don't
-	 * perform the redundant remap_pfn_range() when a PFN already exists.
-	 */
-	if (pfn && pfn_valid(pfn)) {
-		old_pfn = xpmem_vaddr_to_PFN(current->mm, vaddr);
-		if (old_pfn) {
-			if (old_pfn == pfn) {
-				ret = VM_FAULT_NOPAGE;
-			} else {
-				/* should not be possible, but just in case */
-				printk("xpmem_fault_handler: pfn mismatch: "
-					   "%ld != %ld\n", old_pfn, pfn);
-			}
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
-			put_page(pfn_to_page(pfn));
-#else
-			page_cache_release(pfn_to_page(pfn));
-#endif
-			atomic_dec(&seg->tg->n_pinned);
-			atomic_inc(&xpmem_my_part->n_unpinned);
-		} else {
-			XPMEM_DEBUG("calling remap_pfn_range() vaddr=%llx, pfn=%lx",
-						vaddr, pfn);
-			if ((remap_pfn_range(vma, vaddr, pfn, PAGE_SIZE,
-								 vma->vm_page_prot)) == 0) {
-				ret = VM_FAULT_NOPAGE;
-			}
-		}
-	}
-	if (ret == VM_FAULT_SIGBUS) {
-		XPMEM_DEBUG("fault returning SIGBUS vaddr=%llx, pfn=%lx", vaddr, pfn);
-	}
-
-	return ret;
-}
-
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
 static vm_fault_t
 xpmem_fault_handler(struct vm_fault *vmf)
